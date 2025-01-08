0e16551cf1e215237956179cdde37f054fa1d180
DLANDALLY <landally@icloud.com>
2025-01-08 15:49:25 +0100

    creation des controller

---
+++ src/main/java/com/dly/safetynet/controllers/FireStationController.java
@@ -0,0 +1,29 @@
+package com.dly.safetynet.controllers;
+
+import com.dly.safetynet.repositories.FireStationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class FireStationController {
+    @Autowired
+    private FireStationRepository fireStationRepository;
+    /**
+     * http://localhost:8080/firestation?stationNumber=<station_number>
+     * Cette url doit retourner une liste des personnes couvertes par la caserne de pompiers
+     * correspondante. Donc, si le numéro de station = 1, elle doit renvoyer les habitants
+     * couverts par la station numéro 1. La liste doit inclure les informations spécifiques
+     * suivantes : prénom, nom, adresse, numéro de téléphone.
+     * De plus, elle doit fournir un décompte du nombre d'adultes et du nombre d'enfants
+     * (tout individu âgé de 18 ans ou moins) dans la zone desservie.
+     */
+    @GetMapping("/firestation/{stationNumber}=")
+    public void getFireStationById(@PathVariable() Long stationNumber){
+        fireStationRepository.findById(stationNumber);
+    }
+
+}
